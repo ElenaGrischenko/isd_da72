de933677b7c4f8baab178b7bea50e29a4ddaff2f
Kirill <45568332+kandelk@users.noreply.github.com>
2020-09-30 14:41:46 +0300

    feat: initial project commit

---
+++ Kandel_Kirill/Medical/backend/src/test/java/com/university/medical/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
